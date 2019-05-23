8ee532dc234395c8e16d2ac75b61f7e81ad2b9da
Jeff Brannon <piczelspydr@gmail.com>
2019-05-23 18:18:43 +0100

    byte send

--- client.js
+++ client.js
@@ -3,8 +3,20 @@ var net = require('net');
 var client = new net.Socket();
 client.connect(48202, "localhost", function(){
    console.log("connected");
-   client.write("STRING\n");
-    client.write("Additional data.\n");
+
+   var command = parseInt("F2" , 16);
+   var username = getBytes("jeff");
+   var passowrd = getBytes("password1");
+
+    var bytes = ([command]).concat(username).concat(0).concat(passowrd).concat(0);
+    var buff = new Buffer.from(bytes);
+    //var data = buff.toString('utf8');
+    //var bufStream = new BufferStream(buf);
+
+    client.write(buff);
+
+    client.write("data");
+   //client.write("Additional data.\n");
 });
 
 client.on('data', function(data) {
@@ -15,3 +27,29 @@ client.on('data', function(data) {
 client.on('close', function() {
     console.log("disconnected");
 });
+
+client.on('error', function(error) {
+    console.log("Error: " + error);
+    client.destroy();
+});
+
+function getBytes(str) {
+    var bytes = [], char;
+    str = encodeURI(str);
+
+    while (str.length) {
+        char = str.slice(0, 1);
+        str = str.slice(1);
+
+        if ('%' !== char) {
+            bytes.push(char.charCodeAt(0));
+        } else {
+            char = str.slice(0, 2);
+            str = str.slice(2);
+
+            bytes.push(parseInt(char, 16));
+        }
+    }
+
+    return bytes;
+}
